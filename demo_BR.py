9660bd34bc5e1f4105cbad6791570cac4fe5c0c5
Zahid Hasan <smxahsan@gmail.com>
2024-02-26 11:10:50 -0500

    Add files via upload

--- demo_BR.py
+++ demo_BR.py
@@ -15,12 +15,15 @@ import torch
 # from tensorboardX import SummaryWriter
 import torch.nn.functional as F
 random.seed(125)
+from scipy import signal 
 np.random.seed(125)
 
 import visualization
 from visualization import image_show
 
 import pickle
+
+import matplotlib.pyplot as plt
 
 #%%
 
@@ -33,13 +36,7 @@ def model_def():
     B = 1
     S = 8
     N = 16**2 # number of points to track
-    filenames = glob.glob('./demo_images/*.jpg')
-    filenames = sorted(filenames)
-    print('filenames', filenames)
-    max_iters = len(filenames)//S # run each unique subsequence
 
-    log_freq = 2 # when to produce visualizations 
-    
     ## autogen a name
     model_name = "%02d_%d_%d" % (B, S, N)
     model_name += "_%s" % exp_name
@@ -103,24 +100,22 @@ data_br = pickle.load(open("../data.pkl", "rb"))
 
 
 
-def main():
-    fr_gp = 15
-    vid_st = 40
+def main(fr_gp = 40, vid_st = 40, window_ = 40, p = 5):
     data_rs = data_br[::fr_gp]
     xy = None
     
     traj_l , rgbs_l = [], []
     
-    for i in range(30):
+    for i in range(window_):
         
         data_rgbs = data_rs[vid_st:vid_st+8]
         data_rgbs = torch.from_numpy(data_rgbs).permute(0,3,1, 2)
         data_rgbs = data_rgbs.unsqueeze(0)
         # data_rgbs =  torch.cat([data_rgbs, data_rgbs, data_rgbs], dim = 2)
-        rgbs, trajs_e = inferred_value(model, data_rgbs, p = 6, xy = xy)
+        rgbs, trajs_e = inferred_value(model, data_rgbs, p = p, xy = xy)
 
         
-        if i%4 == 0:
+        if i%5 == 0:
             xy = None
         else:
             xy = trajs_e[:,-1,:,:]
@@ -140,21 +135,85 @@ def main():
 #%%
 
 if __name__ == '__main__':
-    trajs_e = main()
+    frame_gap = 10
+    video_start = 11
+    window_ = 20
+    points = 5
+    trajs_e = main(fr_gp=frame_gap, vid_st=video_start, window_= window_, p = points)
     
 #%%
-
-import matplotlib.pyplot as plt
-
 t_s = []
 
-for pos_ in range(36):
+for pos_ in range(points**2):
     v_1,t = [], []
     for i in range(len(trajs_e[0])):
         v_1.append(sum((trajs_e[0,i,pos_] - trajs_e[0,0,pos_])**2).cpu())
-        t.append(i/6)
+        t.append(i*frame_gap/30)
                             
     t_s.append(np.array(v_1))
 
 
 plt.plot(np.array(t), np.array(v_1))
+
+#%% Filter Design
+
+from scipy.signal import butter, lfilter
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+#%% Filtering Process
+
+y = butter_bandpass_filter(np.array(t_s[10]), 0.06, 0.8, 30/frame_gap)
+plt.plot(t,y)
+
+print('zero crossing: ', (np.diff(np.sign(y)) != 0).sum())
+
+
+#%% Frequency response. 
+
+import scipy.fftpack
+
+yf = scipy.fftpack.fft(y)
+
+xf = np.linspace(0.0, 30/(2.0*frame_gap), len(y)//2)
+
+plt.figure()
+
+plt.plot(xf, 2.0/len(y) * np.abs(yf[:len(y)//2]))
+
+
+#%%
+
+t_s_filt = []
+for i in range(len(t_s)):
+    t_s_filt.append(butter_bandpass_filter(np.array(t_s[i]), 0.06, 0.8, 30/frame_gap))
+
+#%% Clustering Algorithms
+
+from tslearn.clustering import KernelKMeans
+gak_km = KernelKMeans(n_clusters=2, kernel="gak")
+X =  np.array(t_s_filt)[:,:, np.newaxis]
+labels_gak = gak_km.fit_predict(X)
+
+
+from tslearn.clustering import TimeSeriesKMeans, silhouette_score
+km = TimeSeriesKMeans(n_clusters=2, metric="dtw")
+labels = km.fit_predict(X)
+silhouette_score(X, labels, metric="dtw")
+
+#%% 
+
+from sklearn.cluster import KMeans
+
+kmeans = KMeans(n_clusters=2).fit(X[:,:,0])
